2d13e9e30a46aa93bae1f6bb03ac55e6f74144d0
ceniatnior <cointrainer1@gmail.com>
2021-12-03 16:16:19 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector(`main`).remove(); 
+
+const newHeader = document.createElement("h1")
+    newHeader.setAttribute("id","victory")
+        document.body.appendChild(newHeader)
+newHeader.innerHTML = "Abraham is the champion" 
